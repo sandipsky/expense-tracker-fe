4d1ec98c781fc7250e8eb21e3f752c5ce0a71948
sandipsky <sandipshakya75@gmail.com>
2025-08-26 16:29:47 +0545

    changes

--- src/app/pages/transaction/transaction.ts
+++ src/app/pages/transaction/transaction.ts
@@ -1,18 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { CommonService } from '../../shared/services/common.service';
 import { Table } from '../../shared/components/table/table';
-import { data } from './data';
 import { SortEvent } from '../../shared/directives/sortable/sortable-header.directive';
 import { ITransaction } from './transaction.model';
+import { PaginatorComponent } from '../../shared/components/paginator/paginator.component';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-transaction',
-  imports: [Table],
+  imports: [Table, PaginatorComponent],
   templateUrl: './transaction.html',
   styleUrl: './transaction.scss'
 })
 export class Transaction {
   public commonService = inject(CommonService);
+  filterData = {
+    pageIndex: 0,
+    pageSize: 25,
+    length: 0
+  }
 
   tableHeaders = [
     { name: 'SN', property: 'sn', sort: false },
@@ -25,17 +31,25 @@ export class Transaction {
     { name: 'Remarks', property: 'remarks', sort: true },
   ];
 
-  tableData: ITransaction[] = data;
+  tableData: ITransaction[] = [];
 
-  constructor() {
-    this.commonService.showSpinner(true);
-
-    setTimeout(() => this.commonService.showSpinner(false), 500)
-  }
+  constructor() {}
 
   onSort(event: SortEvent) {
 
   }
 
+  onPageChange(pageData: PageEvent) {
+    this.filterData.pageIndex = pageData.pageIndex;
+    this.filterData.pageSize = pageData.pageSize;
+    this.commonService.showSpinner(true);
+    setTimeout(() => this.commonService.showSpinner(false), 500)
+
+  }
+
+
+  getTransactionList() {
+
+  }
 
 }
